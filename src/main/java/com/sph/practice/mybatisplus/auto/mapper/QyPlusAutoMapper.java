fbbfeb6a056e48a18487a31953cbf6ee6d040cd5
shenpeihong <shenpeihong@do1.com.cn>
2021-02-19 16:02:57 +0800

    20210219  代码自动生成

---
+++ src/main/java/com/sph/practice/mybatisplus/auto/mapper/QyPlusAutoMapper.java
@@ -0,0 +1,16 @@
+package com.sph.practice.mybatisplus.auto.mapper;
+
+import com.sph.practice.mybatisplus.auto.pojo.QyPlusAuto;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Shen Peihong
+ * @since 2021-02-19
+ */
+public interface QyPlusAutoMapper extends BaseMapper<QyPlusAuto> {
+
+}
---
+++ src/main/java/com/sph/practice/mybatisplus/auto/pojo/QyPlusAuto.java
@@ -0,0 +1,122 @@
+package com.sph.practice.mybatisplus.auto.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.Version;
+import java.time.LocalDateTime;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Shen Peihong
+ * @since 2021-02-19
+ */
+@ApiModel(value="QyPlusAuto对象", description="")
+public class QyPlusAuto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "主键ID")
+    @TableId(value = "id", type = IdType.ID_WORKER)
+    private Long id;
+
+    @ApiModelProperty(value = "账号")
+    private String userName;
+
+    @ApiModelProperty(value = "密码")
+    private String password;
+
+    @ApiModelProperty(value = "逻辑删除字段")
+    @TableLogic
+    private Integer deleted;
+
+    @ApiModelProperty(value = "乐观锁字段")
+    @Version
+    private Integer version;
+
+    @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "修改时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Integer deleted) {
+        this.deleted = deleted;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "QyPlusAuto{" +
+        "id=" + id +
+        ", userName=" + userName +
+        ", password=" + password +
+        ", deleted=" + deleted +
+        ", version=" + version +
+        ", createTime=" + createTime +
+        ", updateTime=" + updateTime +
+        "}";
+    }
+}
